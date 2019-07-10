897de836145a0a80363ba28b8389c91a567bc348
barfiejus <justin.kyle.barfield@gmail.com>
2019-07-10 15:34:07 -0400

    script has begun being written. variable to hold topics and write topics to page as buttons has been added. variables added for giphy url, api key, and parameters. click function added to generate a random giphy using the text of the button clicked as the tag. script added to write pulled giphy to the page.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,19 @@
 // Giphy API key:
 // TZuN5wWmbWU0ZjT6YzH5k5292AUhCHjO
+
+
+// Create generic array of variables
+// Write each variable as a button that calls an ajax query
+// Get each variable to correctly pull from Giphy
+// 
+// Create an input and button on page to create new variables
+// Each variable will make a new button and will pull with that term
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
